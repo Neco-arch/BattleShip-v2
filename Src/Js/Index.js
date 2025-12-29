619b08979a674820693f1a60248ae06ffcd87d5c
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2025-12-29 10:15:52 +0700

    feat : add basic ui for battleship game

--- Src/Js/Index.js
+++
@@ -1,5 +0,0 @@
-import { GameBoard } from "./GameBoards";
-import { PlayerClass } from "./PlayerClass";
-import { ShipFactory } from "./Shipclass";
-
-import "../Style.css"
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { PlayerClass } from "./Js/PlayerClass";
+import { ShipFactory } from "./Js/Shipclass";
+import "./Style.css";
