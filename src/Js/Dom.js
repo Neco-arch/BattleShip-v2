38c6d8efb06e5d4b7c7772d0a0561efb88831ac7
Neco-arch <111620463+Neco-arch@users.noreply.github.com>
2025-12-29 16:01:05 +0700

    feat : change background color when change turn

--- src/Js/Dom.js
+++ src/Js/Dom.js
@@ -1,14 +1,16 @@
 class Dom {
+  constructor() {
+    this.playerBoard = document.querySelector(".Player_Board");
+    this.computerBoard = document.querySelector(".Computer_Board");
+  }
 
-    constructor() {
-        this.playerBoard = document.querySelector(".Player_Board");
-        this.computerBoard = document.querySelector(".Computer_Board");
-    }
+  //CreateBoard
   CreateBoardOnDiv() {
     this.CreateBoard10x10(this.playerBoard);
     this.CreateBoard10x10(this.computerBoard);
   }
 
+  //CreateBoard 10x10
   CreateBoard10x10(Dom_class) {
     for (let i = 0; i < 10; i++) {
       const Column = document.createElement("div");
@@ -24,17 +26,38 @@ class Dom {
     }
   }
 
+  //Change turn (Background Color)
   ChangeTurn(CurrentTurn) {
+    const Player_Side = document.querySelector(".Player_Side");
+    const Computer_Side = document.querySelector(".Computer_Side");
+    if (CurrentTurn === "Player") {
+      Computer_Side.style.backgroundColor = "";
+      Player_Side.style.backgroundColor = "rgba(97, 161, 231, 0.25)";
+      return "Computer";
+    } else if (CurrentTurn === "Computer") {
+      Player_Side.style.backgroundColor = "";
+      Computer_Side.style.backgroundColor = "rgba(97, 161, 231, 0.25)";
+      return "Player";
+    }
 
+    return false;
   }
 
-  PlaceShipOnBoard(Column,Row) {
+  PlaceShipOnBoard(Column, Row) {}
 
+  MakeStartButtondisappearandreappear() {
+    
   }
 }
 
 const Test = new Dom();
 
 Test.CreateBoardOnDiv();
+Test.ChangeTurn("Player");
+
+setTimeout(function () {
+  Test.ChangeTurn("Computer");
+  console.log("Hello");
+}, 5000);
 
 export { Dom };
